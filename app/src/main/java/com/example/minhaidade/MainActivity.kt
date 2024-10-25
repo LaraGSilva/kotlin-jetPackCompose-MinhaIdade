2ee9db6c867f06fad43edfbb6e3352c193f05723
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-24 22:14:34 -0300

    feat: estilizando a funcao CounterScreen

--- app/src/main/java/com/example/minhaidade/MainActivity.kt
+++ app/src/main/java/com/example/minhaidade/MainActivity.kt
@@ -6,14 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.minhaidade.ui.theme.MinhaIdadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,6 +33,7 @@ class MainActivity : ComponentActivity() {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
 
                 }
+                CounterScreen()
             }
         }
     }
@@ -34,19 +43,45 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CounterScreen(){
 
-    Column() {
-        Text(text = "Qual a sua idade? ")
-        Text(text = "Aperte o botao para informar a sua idade.")
-        Text(text = "21")
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "Qual a sua idade? ",
+            fontSize = 24.sp,
+            color= Color.Magenta
+        )
 
-        Row() {
-            Button(onClick = {}) { Text(text = "-") }
-            Button(onClick = {}) { Text(text = "+") }
-           }
+        Text(
+            text = "Aperte o botao para informar a sua idade.",
+            fontSize = 12.sp,
+            color = Color(0xFFAD1F4E)
+
+        )
+
+        Text(
+            text = "21",
+            color = Color.LightGray
+
+        )
+
+        Row {
+            Button(
+                onClick = {},
+                modifier = Modifier.size(84.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(Color.Magenta)
+        ) { Text(text = "-") }
+            Button(
+                onClick = {},
+                modifier = Modifier.size(84.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(Color.Magenta)
+            ) { Text(text = "+") }
         }
-
     }
-
-
-
 }
+
+
+
+
